71e9815ced9ad8b2bb240c59a67c8e21b2ab6457
xschen <xs0040@gmail.com>
2017-10-29 11:35:16 +0800

    source upload

---
+++ cs-optimization-continuous-solutions/ComponentModels/IMOOPop.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ContinuousOptimization.ProblemModels;
+
+namespace ContinuousOptimization.ComponentModels
+{
+    public interface IMOOPop : IPop
+    {
+        IMOOProblem Problem
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ cs-optimization-continuous-solutions/ComponentModels/ISolution.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ContinuousOptimization.ProblemModels;
+
+namespace ContinuousOptimization.ComponentModels
+{
+    public interface ISolution
+    {
+        ISolution Clone();
+    }
+}
